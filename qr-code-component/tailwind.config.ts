12250ece0864825f6047238f92f00dbf52f5839b
Thibaut Edenwald <thibaut@tractr.net>
2024-05-06 13:11:26 -0400

    feat: setup base project

--- qr-code-component/tailwind.config.ts
+++ qr-code-component/tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        "light-gray": "hsl(212, 45%, 89%)",
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+        link: "hsl(228,45%, 44%)",
+      },
+      fontFamily: {
+        sans: ["Outfit"],
+      },
+      fontSize: {
+        base: "15px",
+      },
     },
   },
   plugins: [],
